f601298ee95027ae2e0e7d70126f1e7c7f673a40
DEVADHARSHINI S <727724euai183@skcet.ac.in>
2026-06-22 22:16:47 +0530

    User commit

---
+++ backend/src/main/java/com/example/demo/entity/Flashcard.java
@@ -0,0 +1,47 @@
+ package com.example.demo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="flashcard")
+ public class Flashcard {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String frontContent;
+    private String backContent;
+    private Integer orderIndex;
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getFrontContent() {
+        return frontContent;
+    }
+    public void setFrontContent(String frontContent) {
+        this.frontContent = frontContent;
+    }
+    public String getBackContent() {
+        return backContent;
+    }
+    public void setBackContent(String backContent) {
+        this.backContent = backContent;
+    }
+    public Integer getOrderIndex() {
+        return orderIndex;
+    }
+    public void setOrderIndex(Integer orderIndex) {
+        this.orderIndex = orderIndex;
+    }
+    
+
+
+ 
+    
+ }
