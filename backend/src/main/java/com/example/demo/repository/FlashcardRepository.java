ae28af0166c169a6163d4ce059f94ca3cfa5c34e
DEVADHARSHINI S <727724euai183@skcet.ac.in>
2026-06-24 14:31:19 +0530

    User commit

--- backend/src/main/java/com/example/demo/repository/FlashcardRepository.java
+++ backend/src/main/java/com/example/demo/repository/FlashcardRepository.java
@@ -1,10 +1,8 @@
 package com.example.demo.repository;
 
+import com.example.demo.entity.Flashcard;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public interface FlashcardRepository extends JpaRepository<FlashcardRepository,Long> {
+public interface FlashcardRepository extends JpaRepository<Flashcard, Long> {
 
-    
 }
