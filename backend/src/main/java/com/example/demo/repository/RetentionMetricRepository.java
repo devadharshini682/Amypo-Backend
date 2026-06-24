931b128787fbf8d83f4eb71fb2136de1ab0a0cfa
DEVADHARSHINI S <727724euai183@skcet.ac.in>
2026-06-24 14:32:48 +0530

    User commit

--- backend/src/main/java/com/example/demo/repository/RetentionMetricRepository.java
+++ backend/src/main/java/com/example/demo/repository/RetentionMetricRepository.java
@@ -1,10 +1,8 @@
 package com.example.demo.repository;
 
+import com.example.demo.entity.RetentionMetric;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public interface RetentionMetricRepository extends JpaRepository<RetentionMetricRepository,Long>{
+public interface RetentionMetricRepository extends JpaRepository<RetentionMetric, Long> {
 
-    
 }
--- backend/src/main/java/com/example/demo/repository/StudyDeckRepository.java
+++ backend/src/main/java/com/example/demo/repository/StudyDeckRepository.java
@@ -1,10 +1,8 @@
 package com.example.demo.repository;
 
+import com.example.demo.entity.StudyDeck;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public interface StudyDeckRepository extends JpaRepository<StudyDeckRepository,Long>{
+public interface StudyDeckRepository extends JpaRepository<StudyDeck, Long> {
 
-    
 }
