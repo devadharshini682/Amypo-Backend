ae4e9ec8d6a1710911e5433f44b126b6432d7aaf
DEVADHARSHINI S <727724euai183@skcet.ac.in>
2026-06-24 14:34:18 +0530

    User commit

--- backend/src/main/java/com/example/demo/repository/SystemUserRepository.java
+++ backend/src/main/java/com/example/demo/repository/SystemUserRepository.java
@@ -1,10 +1,8 @@
 package com.example.demo.repository;
 
+import com.example.demo.entity.SystemUser;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public interface SystemUserRepository extends JpaRepository<SystemUserRepository,Long> {
+public interface SystemUserRepository extends JpaRepository<SystemUser, Long> {
 
-    
 }
