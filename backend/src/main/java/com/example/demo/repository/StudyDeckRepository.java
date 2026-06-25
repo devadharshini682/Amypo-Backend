1899692414c9753e823b9a0039bce55fe2970614
DEVADHARSHINI S <727724euai183@skcet.ac.in>
2026-06-25 08:27:13 +0530

    User commit

--- backend/src/main/java/com/example/demo/repository/StudyDeckRepository.java
+++ backend/src/main/java/com/example/demo/repository/StudyDeckRepository.java
@@ -2,7 +2,9 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.StudyDeck;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface StudyDeckRepository extends JpaRepository<StudyDeck, Long> {
 
 }
