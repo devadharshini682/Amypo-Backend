085a0ce3e1589fb393e732b6c6b4d15b1062c9f9
DEVADHARSHINI S <727724euai183@skcet.ac.in>
2026-06-25 08:24:13 +0530

    User commit

--- backend/src/main/java/com/example/demo/entity/LanguageTrack.java
+++ backend/src/main/java/com/example/demo/entity/LanguageTrack.java
@@ -1,6 +1,23 @@
 package com.example.demo.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "language_track")
 public class LanguageTrack {
 
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    public LanguageTrack() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
