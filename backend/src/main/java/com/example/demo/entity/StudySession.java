fda22f114a0dca688d0b7a9dae726a89853659ec
DEVADHARSHINI S <727724euai183@skcet.ac.in>
2026-07-01 21:26:58 +0530

    User commit

--- backend/src/main/java/com/example/demo/entity/StudySession.java
+++ backend/src/main/java/com/example/demo/entity/StudySession.java
@@ -1,24 +1,39 @@
 package com.example.demo.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+
 import java.time.LocalDateTime;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-
 @Entity
+@Table(name = "study_session")
 public class StudySession {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private LocalDateTime startTime;
+
     private LocalDateTime endTime;
+
+    @Min(0)
+    @Max(5)
     private Integer score;
+
     private Long userId;
+
     private Long deckId;
-    public StudySession(Long id, LocalDateTime startTime, LocalDateTime endTime, Integer score, Long userId,
-            Long deckId) {
+
+    public StudySession() {
+    }
+
+    public StudySession(Long id, LocalDateTime startTime,
+                        LocalDateTime endTime,
+                        Integer score,
+                        Long userId,
+                        Long deckId) {
         this.id = id;
         this.startTime = startTime;
         this.endTime = endTime;
@@ -26,47 +41,52 @@ public class StudySession {
         this.userId = userId;
         this.deckId = deckId;
     }
+
     public Long getId() {
         return id;
     }
+
     public void setId(Long id) {
         this.id = id;
     }
+
     public LocalDateTime getStartTime() {
         return startTime;
     }
+
     public void setStartTime(LocalDateTime startTime) {
         this.startTime = startTime;
     }
+
     public LocalDateTime getEndTime() {
         return endTime;
     }
+
     public void setEndTime(LocalDateTime endTime) {
         this.endTime = endTime;
     }
+
     public Integer getScore() {
         return score;
     }
+
     public void setScore(Integer score) {
         this.score = score;
     }
+
     public Long getUserId() {
         return userId;
     }
+
     public void setUserId(Long userId) {
         this.userId = userId;
     }
+
     public Long getDeckId() {
         return deckId;
     }
+
     public void setDeckId(Long deckId) {
         this.deckId = deckId;
     }
-    
-
-
-
-
-
-    
 }
