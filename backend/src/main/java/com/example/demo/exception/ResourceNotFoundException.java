8fe9fbea3f202fc931fac968741ee069f400c6c2
DEVADHARSHINI S <727724euai183@skcet.ac.in>
2026-07-03 10:08:53 +0530

    User commit

--- backend/src/main/java/com/example/demo/exception/ResourceNotFoundException.java
+++ backend/src/main/java/com/example/demo/exception/ResourceNotFoundException.java
@@ -1,8 +1,10 @@
 package com.example.demo.exception;
 
-public class ResourceNotFoundException extends RuntimeException {
+public class ResourceNotFoundException extends RuntimeException { //converting java class into exception class
 
+
+    //parameterised constructor
     public ResourceNotFoundException(String message) {
-        super(message);
+        super(message); 
     }
 }
