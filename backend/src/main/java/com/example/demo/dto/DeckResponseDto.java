c6abd7ffc0e81f01ce812590aa1b8c9f1fbedb60
DEVADHARSHINI S <727724euai183@skcet.ac.in>
2026-07-04 19:13:00 +0530

    User commit

--- backend/src/main/java/com/example/demo/dto/DeckResponseDto.java
+++ backend/src/main/java/com/example/demo/dto/DeckResponseDto.java
@@ -1,14 +1,23 @@
 package com.example.demo.dto;
 
-public class DeckRequestDto {
+public class DeckResponseDto {
 
+    private Long id;
     private String title;
     private String description;
     private Integer capacity;
     private String mentorName;
     private Long ownerId;
 
-    public DeckRequestDto() {
+    public DeckResponseDto() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getTitle() {
