5e634e5451631fe79400297fa22ac13708cea1fd
DEVADHARSHINI S <727724euai183@skcet.ac.in>
2026-07-08 22:08:23 +0530

    User commit

--- backend/src/main/java/com/example/demo/repository/StudyDeckRepository.java
+++ backend/src/main/java/com/example/demo/repository/StudyDeckRepository.java
@@ -1,10 +1,21 @@
-package com.example.demo.repository;
+ package com.example.demo.repository;
+
+// import com.example.demo.entity.StudyDeck;
+// import org.springframework.data.jpa.repository.JpaRepository;
+// import org.springframework.stereotype.Repository;
+
+// @Repository
+// public interface StudyDeckRepository extends JpaRepository<StudyDeck, Long> {
+
+// }
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.entity.StudyDeck;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public interface StudyDeckRepository extends JpaRepository<StudyDeck, Long> {
+public interface StudyDeckRepository extends JpaRepository<StudyDeck, Long>{
+
+    List<StudyDeck> findByTitleContaining(String keyword);
 
 }
