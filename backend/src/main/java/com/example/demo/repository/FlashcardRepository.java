837e6995c475a19f3c94a15e99672cc33b95e3da
DEVADHARSHINI S <727724euai183@skcet.ac.in>
2026-07-18 15:02:07 +0530

    User commit

--- backend/src/main/java/com/example/demo/repository/FlashcardRepository.java
+++ backend/src/main/java/com/example/demo/repository/FlashcardRepository.java
@@ -1,9 +1,25 @@
+// package com.example.demo.repository;
+
+
+
+// import com.example.demo.entity.Flashcard;
+// import org.springframework.data.jpa.repository.JpaRepository;
+// import org.springframework.stereotype.Repository;
+
+// import java.util.List;
+
+// @Repository
+// public interface FlashcardRepository extends JpaRepository<Flashcard, Long> {
+
+//     List<Flashcard> findByStudyDeckIdOrderByOrderIndexAsc(Long deckId);
+
+// }
+
 package com.example.demo.repository;
-
-
 
 import com.example.demo.entity.Flashcard;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,5 +28,8 @@ import java.util.List;
 public interface FlashcardRepository extends JpaRepository<Flashcard, Long> {
 
     List<Flashcard> findByStudyDeckIdOrderByOrderIndexAsc(Long deckId);
+
+    @Query("SELECT f FROM Flashcard f WHERE f.studyDeck.id = ?1")
+    List<Flashcard> findFlashcardsByDeckId(Long deckId);
 
 }
