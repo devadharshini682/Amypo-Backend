0d3b521b3e8037e3e075bb8ab1471ff416fab075
DEVADHARSHINI S <727724euai183@skcet.ac.in>
2026-07-19 15:40:14 +0530

    User commit

--- backend/src/main/java/com/example/demo/config/AppConfig.java
+++ backend/src/main/java/com/example/demo/config/AppConfig.java
@@ -1,18 +1,4 @@
-// package com.example.demo.config;
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-// import org.springframework.security.crypto.password.PasswordEncoder;
 
-// @Configuration
-// public class AppConfig {
-
-//     @Bean
-//     public PasswordEncoder passwordEncoder() {
-//         return new BCryptPasswordEncoder();
-//     }
-
-// }
 package com.example.demo.config;
 
 import org.springframework.context.annotation.Configuration;
