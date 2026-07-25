0aa02918edbc65f4c096e25f29d389dfd4637e38
DEVADHARSHINI S <727724euai183@skcet.ac.in>
2026-07-25 12:34:44 +0530

    User commit

--- backend/src/main/java/com/example/demo/repository/SystemUserRepository.java
+++ backend/src/main/java/com/example/demo/repository/SystemUserRepository.java
@@ -1,4 +1,20 @@
+// package com.example.demo.repository;
+// import com.example.demo.entity.SystemUser;
+// import org.springframework.data.jpa.repository.JpaRepository;
+// import org.springframework.stereotype.Repository;
+
+// import java.util.Optional;
+
+// @Repository
+// public interface SystemUserRepository extends JpaRepository<SystemUser, Long> {
+
+//     Optional<SystemUser> findByUsername(String username);
+
+//     boolean existsByUsername(String username);
+
+// }
 package com.example.demo.repository;
+
 import com.example.demo.entity.SystemUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -12,4 +28,5 @@ public interface SystemUserRepository extends JpaRepository<SystemUser, Long> {
 
     boolean existsByUsername(String username);
 
+    boolean existsByEmail(String email);
 }
