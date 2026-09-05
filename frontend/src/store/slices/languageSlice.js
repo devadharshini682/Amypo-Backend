d1e38c36ae54a734f43cdb6ebdf0ca0be6233029
DEVADHARSHINI S <727724euai183@skcet.ac.in>
2026-09-05 08:29:48 +0530

    User commit

--- frontend/src/store/slices/languageSlice.js
+++ frontend/src/store/slices/languageSlice.js
@@ -1,46 +1,4 @@
-// import { createSlice } from "@reduxjs/toolkit";
 
-// const initialState = {
-//   languages: [],
-//   selectedLanguage: null,
-//   loading: false,
-//   error: null,
-// };
-
-// const languageSlice = createSlice({
-//   name: "languages",
-
-//   initialState,
-
-//   reducers: {
-//     setLanguages: (state, action) => {
-//       state.languages = action.payload;
-//       state.loading = false;
-//     },
-
-//     setSelectedLanguage: (state, action) => {
-//       state.selectedLanguage = action.payload;
-//     },
-
-//     setLanguageLoading: (state, action) => {
-//       state.loading = action.payload;
-//     },
-
-//     setLanguageError: (state, action) => {
-//       state.error = action.payload;
-//       state.loading = false;
-//     },
-//   },
-// });
-
-// export const {
-//   setLanguages,
-//   setSelectedLanguage,
-//   setLanguageLoading,
-//   setLanguageError,
-// } = languageSlice.actions;
-
-// export default languageSlice.reducer;
 import { createSlice } from "@reduxjs/toolkit";
 
 const languageSlice = createSlice({
